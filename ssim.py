b0949040ab4a02813c6c3ccf2ca1c82e72ded951
thanh0702 <thanhkt2k4@gmail.com>
2025-03-12 22:46:58 +0700

    Update ssim.py

--- ssim.py
+++ ssim.py
@@ -2,7 +2,7 @@ from skimage.metrics import structural_similarity as ssim
 import cv2
 
 # Đọc ảnh gốc và ảnh đã xử lý
-original_image = cv2.imread("D:/srcnn/data/sutu.jpg")
+original_image = cv2.imread("D:/srcnn/data/vit.jpg")
 processed_image = cv2.imread("D:/srcnn/data/output.jpg")
 
 # Kiểm tra xem ảnh có được đọc thành công không
@@ -19,6 +19,15 @@ processed_image = cv2.cvtColor(processed_image, cv2.COLOR_BGR2RGB)
 ssim_red = ssim(original_image[:, :, 0], processed_image[:, :, 0], data_range=255)
 ssim_green = ssim(original_image[:, :, 1], processed_image[:, :, 1], data_range=255)
 ssim_blue = ssim(original_image[:, :, 2], processed_image[:, :, 2], data_range=255)
+
+# Chuyển đổi ảnh sang thang độ xám
+original_gray = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+processed_gray = cv2.cvtColor(processed_image, cv2.COLOR_BGR2GRAY)
+
+# Tính toán SSIM giữa hai ảnh
+ssim_value = ssim(original_gray, processed_gray)
+
+print(f'Chỉ số SSIM giữa hai ảnh xám: {ssim_value}')
 
 # Tính trung bình SSIM cho cả ba kênh màu
 ssim_mean = (ssim_red + ssim_green + ssim_blue) / 3
